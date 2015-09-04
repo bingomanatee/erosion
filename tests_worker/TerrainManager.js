85525b9054f869ba2d2b6f8bcdecfbf9a538a4d5
Dave Edelhart <dave@wonderlandlabs.com>
2015-09-03 19:27:12 -0700

    ocnverting to cluster-hub

---
+++ tests_worker/TerrainManager.js
@@ -0,0 +1,32 @@
+var Terrain = require('./../lib/Terrain');
+var TerrainManager = require('./../lib/TerrainManager');
+var cluster = require('cluster');
+var tap = require('tap');
+
+if (cluster.isWorker){
+    var Hub = require('cluster-hub');
+    var hub = new Hub();
+
+    hub.on('initDivision',  function(data, sender, callback){
+        callback(null, true);
+    });
+
+    hub.on('die', process.exit.bind(process));
+
+} else {
+    var terr = new Terrain(12, 12, 5);
+
+    var mgr = new TerrainManager(terr);
+
+    tap.test('manager init', function(t){
+       mgr.init().
+         then(function(){
+             mgr.divisions.forEach(function(div){
+                tap.ok(div.worker);
+                 mgr.hub.sendToWorkers('die');
+                 t.end();
+             });
+
+         })
+    });
+}
