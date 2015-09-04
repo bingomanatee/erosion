e769965e360fbbcb4556950746074a9ea6c64bbb
Dave Edelhart <dave@wonderlandlabs.com>
2015-09-04 08:13:43 -0700

    work with bounds messaging

--- tests_worker/TerrainWorker.js
+++ tests_worker/TerrainWorker.js
@@ -10,16 +10,30 @@ if (cluster.isWorker) {
 
 } else {
 
-    var terr = new Terrain(12, 12, 5);
+    var terr = new Terrain(12, 10, function(i, j){ return i * j;});
+    var divisions = terr.divide(2);
+    var div1 = divisions[0];
+
     var Hub = require('cluster-hub');
     var hub = new Hub();
+    var BOUNDS_STUB = 'foo';
 
     tap.test('#createDivision', function (t) {
         var worker = cluster.fork();
-        hub.requestWorker(worker, 'initDivision', terr.toData().toString(), function (er, result) {
-            t.similar(result, terr.toJSON(), 'worker echoes terrain data');
-            hub.requestWorker(worker, 'die', {}, function () {
-                t.end();
+
+        hub.on('getBounds', function(data, sender, callback){
+            t.similar(data, div1.toJSON(), 'getBounds is polled with the worker dimensions');
+            callback(null, BOUNDS_STUB);
+        });
+
+        hub.requestWorker(worker, 'initDivision', div1.toData().toString('hex'), function (er, result) {
+            t.similar(result, div1.toJSON(true), 'worker echoes terrain data');
+
+            hub.requestWorker(worker, 'initBounds', {}, function(er, result){
+                t.similar(result, BOUNDS_STUB, 'result is bounds stub');
+                hub.requestWorker(worker, 'die', {}, function () {
+                    t.end();
+                });
             });
         });
     });
