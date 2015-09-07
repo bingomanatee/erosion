7f83030218904c920fb4acc53af56088167dda83
Dave Edelhart <dave@wonderlandlabs.com>
2015-09-07 11:35:59 -0700

    validated smooting worker action

--- test_scripts/smooth.js
+++ test_scripts/smooth.js
@@ -1,12 +1,21 @@
 var q = require('q');
+var util = require('util');
 
 function _smooth(worker) {
+    //console.log(':::::::::: smoothing ', worker.terrain.toJSON());
+    //console.log(worker.terrain.to2Darray());
+    var startTime = new Date().getTime();
     worker.terrain.updateTerrain(function (cell) {
-        console.log('smoothing', cell.i, cell.j, cell.value);
+        //  console.log(' ================ smoothing', cell.i, cell.j, cell.value, 'of', worker.terrain.toJSON());
         var neighbors = cell.neighbors();
+        if (!neighbors.length) {
+            console.log('no neighbors for ', cell.i, cell.j);
+            console.log('... _neighbors =', cell._neighbors);
+        }
         var neighborHeight = neighbors.reduce(function (out, cell) {
             var h = cell.height();
             if (h === null) {
+                //     console.log('no value at ', cell.i, cell.j );
                 return out;
             }
             ++out.count;
@@ -16,13 +25,15 @@ function _smooth(worker) {
         }, {count: 0, sum: 0});
         if (neighborHeight.count <= 0) {
             console.log('no counts???');
-            return 0;
+            console.log(util.inspect(cell, {depth: 3}));
+            return -1;
         }
-        var mean = Math.floor(neighborHeight.sum / neighborHeight.count);
-        return mean;
+        return Math.floor(neighborHeight.sum / neighborHeight.count);
     });
-    console.log('smoothed terrain for ', worker.terrain.toJSON());
-    console.log(worker.terrain.to2Darray());
+    //console.log(' ::::::::::::::::::::::: smoothed terrain for ', worker.terrain.toJSON(), 'in',
+    //  (new Date().getTime() - startTime) / 1000, 'secs'
+    //);
+    //console.log(worker.terrain.to2Darray());
 }
 
 module.exports = function (worker, noBounds) {
--- tests_worker/integrated.js
+++ tests_worker/integrated.js
@@ -14,28 +14,53 @@ if (cluster.isWorker) {
         return Math.pow(++n, 2);
     });
 
-    console.log('terrain: ', ter.toJSON());
-    console.log('terrain cells:');
-    console.log(ter.to2Darray());
-    console.log('ter data: ', ter.toData('hex'));
+    //console.log('terrain: ', ter.toJSON());
+    // console.log('terrain cells:');
+    // console.log(ter.to2Darray());
+    // console.log('ter data: ', ter.toData());
 
-    var manager = new TerrainManager(ter);
-    manager.init(2)
-      .then(function () {
-          console.log('initialized');
-          var path = require('path');
-          var smoothScript = path.resolve(__dirname, './../test_scripts/smooth');
+    tap.test('integrated test of updating worker and master terrain', function (t) {
 
-          console.log('updating workers');
-          manager.updateWorkers(smoothScript).then(function () {
-              setTimeout(function () {
-                  console.log('close workers');
+        var manager = new TerrainManager(ter);
+        manager.init(2)
+          .then(function () {
+              //  console.log('initialized');
+              var startTime = new Date().getTime();
+              var path = require('path');
+              var smoothScript = path.resolve(__dirname, './../test_scripts/smooth');
+              //  console.log('updating workers');
+
+              manager.updateWorkers(smoothScript, true).then(function () {
+                  var data = manager.masterTerrain.to2Darray();
+
+                  //   console.log('final data: ', data);
+                  t.match(data, [[89, 88, 105, 124, 145, 168, 193, 220, 249, 280],
+                        [214, 219, 244, 271, 300, 331, 364, 399, 436, 456],
+                        [546, 559, 604, 651, 700, 751, 804, 859, 916, 944],
+                        [1078, 1099, 1164, 1231, 1300, 1371, 1444, 1519, 1596, 1632],
+                        [1810, 1839, 1924, 2011, 2100, 2191, 2284, 2379, 2476, 2520],
+                        [2049, 2140, 2233, 2328, 2425, 2524, 2625, 2728, 2833, 2794],
+                        [4689, 4648, 4785, 4924, 5065, 5208, 5353, 5500, 5649, 5800],
+                        [5206, 5259, 5404, 5551, 5700, 5851, 6004, 6159, 6316, 6384],
+                        [6738, 6799, 6964, 7131, 7300, 7471, 7644, 7819, 7996, 8072],
+                        [8470, 8539, 8724, 8911, 9100, 9291, 9484, 9679, 9876, 9960],
+                        [10402, 10479, 10684, 10891, 11100, 11311, 11524, 11739, 11956, 12048],
+                        [11049, 11260, 11473, 11688, 11905, 12124, 12345, 12568, 12793, 12714]],
+                    'smoothed workers changed terrain');
+
+                  var elapsed = (new Date().getTime() - startTime) / 1000;
+                  t.ok(elapsed < 1, 'took less than a second');
+
                   manager.closeWorkers().then(function () {
-                      console.log('closed');
-                      process.exit();
-                  })
-              }, 4000);
+                      //    console.log('closed');
+                      t.end();
+                  }, function (err) {
+                      console.log('error on close: ', err);
+                  });
+              }, function (err) {
+                  console.log('error from updateWorkers: ', err);
+              });
           });
-      });
+    });
 
 }
